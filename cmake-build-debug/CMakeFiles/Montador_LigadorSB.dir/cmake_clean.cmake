6fdb3c9fb8ad94e2a805945550d9d0ca06200d90
vinicoreia <shadowtrolling@gmail.com>
2017-04-30 19:14:03 -0300

    primeiro commit

---
+++ cmake-build-debug/CMakeFiles/Montador_LigadorSB.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Montador_LigadorSB.dir/main.cpp.o"
+  "Montador_LigadorSB.pdb"
+  "Montador_LigadorSB"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Montador_LigadorSB.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
