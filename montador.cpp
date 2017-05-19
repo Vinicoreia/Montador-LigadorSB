e35173cc5a424c62bff63bc86d2dcba8ad245ffc
vinicoreia <shadowtrolling@gmail.com>
2017-05-19 00:37:34 -0300

    procura rotulo na primeira passagem e checa se rotulo eh valido

--- montador.cpp
+++ montador.cpp
@@ -16,34 +16,37 @@
 #include <algorithm>
 #include <vector>
 #include <numeric>
-using namespace std;
 
-typedef struct{
+using namespace std;
+int numLinha;
+int flagErros = -1;
+typedef struct {
     string instrucaoOuDiretiva;
     int operando;
-}tabInstrucaoOuDiretiva;
+} tabInstrucaoOuDiretiva;
 
-typedef struct{
+typedef struct {
     string simbolo;
     int posicao;
     int externo; /*0 ou 1*/
     int secdados;
-}tabSimbolos;
+} tabSimbolos;
 
-typedef struct{
+typedef struct {
     string simbolo;
     int posicao;
-}tabUso;
+} tabUso;
 
-typedef struct{
+typedef struct {
     string simbolo;
     int posicao;
-}tabDef;
+} tabDef;
 
 /*
  * Como o número de instruções e diretivas é pequeno resolvemos criar a tabela ao invés de ler a tabela de um arquivo.
  * */
-void GeraTabelaInstrucoesEDiretivas(std::vector<tabInstrucaoOuDiretiva> tabInstrucao, std::vector<tabInstrucaoOuDiretiva> tabDiretiva){
+void GeraTabelaInstrucoesEDiretivas(std::vector<tabInstrucaoOuDiretiva> tabInstrucao,
+                                    std::vector<tabInstrucaoOuDiretiva> tabDiretiva) {
     /*
      * VETOR DE DIRETIVAS
      * */
@@ -51,90 +54,139 @@ void GeraTabelaInstrucoesEDiretivas(std::vector<tabInstrucaoOuDiretiva> tabInstr
     tabDiretiva[0].operando = 0;
     tabInstrucao[1].instrucaoOuDiretiva = "SPACE";
     tabDiretiva[1].operando = 1;
-    tabDiretiva[2].instrucaoOuDiretiva ="CONST";
+    tabDiretiva[2].instrucaoOuDiretiva = "CONST";
     tabDiretiva[2].operando = 1;
-    tabDiretiva[3].instrucaoOuDiretiva ="EQU";
+    tabDiretiva[3].instrucaoOuDiretiva = "EQU";
     tabDiretiva[3].operando = 1;
-    tabDiretiva[4].instrucaoOuDiretiva ="IF";
+    tabDiretiva[4].instrucaoOuDiretiva = "IF";
     tabDiretiva[4].operando = 1;
-    tabDiretiva[5].instrucaoOuDiretiva ="BEGIN";
+    tabDiretiva[5].instrucaoOuDiretiva = "BEGIN";
     tabDiretiva[5].operando = 0;
-    tabDiretiva[6].instrucaoOuDiretiva ="END";
+    tabDiretiva[6].instrucaoOuDiretiva = "END";
     tabDiretiva[6].operando = 0;
-    tabDiretiva[7].instrucaoOuDiretiva ="PUBLIC";
+    tabDiretiva[7].instrucaoOuDiretiva = "PUBLIC";
     tabDiretiva[7].operando = 0;
-    tabDiretiva[8].instrucaoOuDiretiva ="EXTERN";
+    tabDiretiva[8].instrucaoOuDiretiva = "EXTERN";
     tabDiretiva[8].operando = 0;
 
     /* Inicializando as instruções como um vetor de struct permite que o código da instrução seja o indice+1*/
     /*Da mesma maneira o tamaho da instrução será o número de operandos + 1*/
-    tabInstrucao[0].instrucaoOuDiretiva ="ADD";
+    tabInstrucao[0].instrucaoOuDiretiva = "ADD";
     tabInstrucao[0].operando = 1;
-    tabInstrucao[1].instrucaoOuDiretiva ="SUB";
+    tabInstrucao[1].instrucaoOuDiretiva = "SUB";
     tabInstrucao[1].operando = 1;
-    tabInstrucao[2].instrucaoOuDiretiva ="MULT";
+    tabInstrucao[2].instrucaoOuDiretiva = "MULT";
     tabInstrucao[2].operando = 1;
-    tabInstrucao[3].instrucaoOuDiretiva ="DIV";
+    tabInstrucao[3].instrucaoOuDiretiva = "DIV";
     tabInstrucao[3].operando = 1;
-    tabInstrucao[4].instrucaoOuDiretiva ="JMP";
+    tabInstrucao[4].instrucaoOuDiretiva = "JMP";
     tabInstrucao[4].operando = 1;
-    tabInstrucao[5].instrucaoOuDiretiva ="JMPN";
+    tabInstrucao[5].instrucaoOuDiretiva = "JMPN";
     tabInstrucao[5].operando = 1;
-    tabInstrucao[6].instrucaoOuDiretiva ="JMPP";
+    tabInstrucao[6].instrucaoOuDiretiva = "JMPP";
     tabInstrucao[6].operando = 1;
-    tabInstrucao[7].instrucaoOuDiretiva ="JMPZ";
+    tabInstrucao[7].instrucaoOuDiretiva = "JMPZ";
     tabInstrucao[7].operando = 1;
-    tabInstrucao[8].instrucaoOuDiretiva ="COPY";
+    tabInstrucao[8].instrucaoOuDiretiva = "COPY";
     tabInstrucao[8].operando = 2;
-    tabInstrucao[9].instrucaoOuDiretiva ="LOAD";
+    tabInstrucao[9].instrucaoOuDiretiva = "LOAD";
     tabInstrucao[9].operando = 1;
-    tabInstrucao[10].instrucaoOuDiretiva ="STORE";
+    tabInstrucao[10].instrucaoOuDiretiva = "STORE";
     tabInstrucao[10].operando = 1;
-    tabInstrucao[11].instrucaoOuDiretiva ="INPUT";
+    tabInstrucao[11].instrucaoOuDiretiva = "INPUT";
     tabInstrucao[11].operando = 1;
-    tabInstrucao[12].instrucaoOuDiretiva ="OUTPUT";
+    tabInstrucao[12].instrucaoOuDiretiva = "OUTPUT";
     tabInstrucao[12].operando = 1;
-    tabInstrucao[13].instrucaoOuDiretiva ="STOP";
+    tabInstrucao[13].instrucaoOuDiretiva = "STOP";
     tabInstrucao[13].operando = 0;
 }
 
-int VerificaSeLinhaValida(std::string checaCaracter ){
+size_t VerificaSeLinhaValida(string checaCaracter) {
     /*Lembrar que aqui a linha ja não é sensível ao caso
      *
      */
-    int posicao =(int)checaCaracter.find_first_not_of("ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789+-*/=|&!?#%(){}[]_\'\",.;<>");
-    if (posicao >-1) {
+    size_t posicao = checaCaracter.find_first_not_of("ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789+-*/=|&!?#%(){}[]_\'\",.;<>");
+    if (posicao != string::npos) {
+        cout << "Erro lexico na linha " << numLinha << " caractere invalido";
         return posicao;
-    }
+    } else
+        return 10000; // retorno se não existe caractere especial
 }
+
 // como vetor podemos usar a funcao find
-int PesquisaInstrucaoEDiretiva (std::string instrucaoOuDiretiva, std::vector<tabInstrucaoOuDiretiva> vetorInstOuDiretiva) {
+int
+PesquisaInstrucaoEDiretiva(std::string instrucaoOuDiretiva, std::vector<tabInstrucaoOuDiretiva> vetorInstOuDiretiva) {
     vector<tabInstrucaoOuDiretiva>::iterator it;
     it = find(vetorInstOuDiretiva.begin(), vetorInstOuDiretiva.end(), instrucaoOuDiretiva);
-    if(it!= vetorInstOuDiretiva.end())
-        return (int)distance(vetorInstOuDiretiva.begin(), it); // retorna a posicao da instrucao ou diretiva
+    if (it != vetorInstOuDiretiva.end())
+        return (int) distance(vetorInstOuDiretiva.begin(), it); // retorna a posicao da instrucao ou diretiva
     else
         return -1;
 }
+
 // a pesquisa nao pode ser igual pois os tipos dos iteradores sao diferentes
-int PesquisaSimbolo (std::string simbolo, std::vector<tabSimbolos> vetorSimbolos) {
+int PesquisaSimbolo(std::string simbolo, std::vector<tabSimbolos> vetorSimbolos) {
     vector<tabSimbolos>::iterator it;
     it = find(vetorSimbolos.begin(), vetorSimbolos.end(), simbolo);
-    if(it!= vetorSimbolos.end())
-        return (int)distance(vetorSimbolos.begin(), it); // retorna a posicao da instrucao ou diretiva
+    if (it != vetorSimbolos.end())
+        return (int) distance(vetorSimbolos.begin(), it); // retorna a posicao da instrucao ou diretiva
     else
         return -1;
 }
 
+bool ProcuraRotulo(string linha) {
+    size_t posicao = linha.find_first_of(":");
+    return posicao != string::npos;
+}
 
-int main_montador (){
+void checaSeRotuloValido(string linha) {
+    //  se o rotulo tem mais de 100 caracteres
+    if (linha.size()>100){
+        cout << "Erro Sintatico na linha " << numLinha << " Primeira letra do rotulo nao pode ser numero";
+    }
+    // se rotulo comeca com numero
+    if (linha.find_first_of("0123456789") == 0) {
+        cout << "Erro Sintatico na linha " << numLinha << " Tamanho maximo permitido pro rotulo eh 100 caracteres";
+    }
+}
+
+void PrimeiraPassagem(string linha, vector<tabSimbolos> vetorSimbolos, vector<tabDef> vetorDefinicoes,
+                      vector<tabUso> tabelaUso) {
+    int i = 0, j = 0, chartoint = 0;
+    int retorno;
+    char c;
+    string aux;
+    string SPACE = "SPACE";
+    string EXTERN = "EXTERN";
+    string PUBLIC = "PUBLIC";
+    string SECTION = "SECTION";
+    vector<tabInstrucaoOuDiretiva> vetorInstrucao;
+    vector<tabInstrucaoOuDiretiva> vetorDiretiva;
+    flagErros = (int) VerificaSeLinhaValida(linha);
+    GeraTabelaInstrucoesEDiretivas(vetorInstrucao, vetorDiretiva);
+
+    if (ProcuraRotulo(linha)) {
+        checaSeRotuloValido(linha);
+
+    }
+
+    //primeiramente vamos achar os rotulos
+
+}
+
+
+int main_montador() {
     //primeiro vou escrever a ideia
     //recebe o arquivo preprocessado;
     fstream assembly;
     int contador_posicao = 0;
     int contador_linha = 1;
-
-
+    /*vector<tabSimbolos> vetorSimbolos;
+    vector<tabDef> vetorDef;
+    vector<tabUso> vetorUso;
+    char c;
+    char string[1000], codigoObjeto[1000], TU[1000], TD[1000];
+    int i, buff = 1;*/
     string linha;
     while (getline(assembly, linha)) {
         // aqui eu leio linha por linha do programa;
@@ -153,7 +205,7 @@ int main_montador (){
          *  senao: erro, operacao não identificada.
          *
          * */
-        contador_linha ++;
+        contador_linha++;
 
-        }
+    }
 }
